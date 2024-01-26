6e0d026525570f1ec3b1085302307c26c045c1a2
2220645 <129009945+2220645@users.noreply.github.com>
2024-01-26 21:01:02 +0600

    Added demo file

---
+++ BasicArithmeticOparation/src/Main/BasicArithmeticOparation.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Main;
+
+
+public class BasicArithmeticOparation {
+
+    
+    public static void main(String[] args) {
+        int Number1=10;
+        int Number2=11;
+        
+        float Avarage=(float)(Number1+Number2)/2;
+        System.out.println("Avarage"+Avarage);
+       
+    }
+    
+}
